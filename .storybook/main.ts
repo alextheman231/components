c0ee772301ce4e353a0c7da384bbce25e4c308e8
AlexMan123456 <alex.man03888@gmail.com>
2026-03-17 23:21:34 +0000

    Move the stories folder out of src and into the root
    I think this aligns better with most of my other practices, where our project folders represent the different responsibilities of the project. In my mental model, src represents the source code files - they are the thing the user directly interacts with. The tests folder is then for code tests that validate the source code, the docs is for documentation to explain what the source code does, and then the new stories folder acts as a playground environment to try out the source code, in a less rigourous environment than the tests.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,12 +5,13 @@ import { fileURLToPath } from "node:url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   viteFinal: async (config) => {
     config.resolve = config.resolve ?? {};
     config.resolve.alias = {
       ...(config.resolve.alias ?? {}),
-      src: path.resolve(__dirname, "../src"),
+      src: path.resolve(__dirname, "..", "src"),
+      stories: path.resolve(__dirname, "..", "stories"),
     };
     return config;
   },
