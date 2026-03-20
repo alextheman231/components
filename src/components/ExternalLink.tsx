0134a4767b526f0fd65a1ad20f4ef93ec3862b76
AlexMan123456 <alex.man03888@gmail.com>
2026-03-20 01:08:02 +0000

    Refactor the stories to use the render property on the story so that the JSDoc comments/types resolve correctly in the docs.

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -4,12 +4,20 @@ import type { ReactNode, Ref } from "react";
 import MUILink from "@mui/material/Link";
 
 export interface ExternalLinkProps extends Omit<LinkProps, "to" | "target" | "rel"> {
+  /** The URL of the place you want to navigate to. */
   href: `https://${string}` | `http://${string}` | (string & {});
   to?: never;
+  /** The readable content to display on the link. */
   children: ReactNode;
+  /** An optional ref to allow it to be used with polymorphic components. */
   ref?: Ref<HTMLAnchorElement>;
 }
 
+/**
+ * A stylised link that is best used when you want to navigate to a different domain.
+ *
+ * Opens the destination in a new tab and applies recommended security defaults automatically.
+ */
 function ExternalLink({ href, children, ref, ...linkProps }: ExternalLinkProps) {
   return (
     <MUILink
--- stories/ExternalLink/ExternalLink.stories.tsx
+++ stories/ExternalLink/ExternalLink.stories.tsx
@@ -1,15 +1,19 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
+import { ExternalLink } from "src";
 import ExternalLinkMain from "stories/ExternalLink/demos/ExternalLinkMain";
 
-const meta: Meta<typeof ExternalLinkMain> = {
-  component: ExternalLinkMain,
+const meta: Meta<typeof ExternalLink> = {
+  component: ExternalLink,
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Main: Story = {
+  render: (props) => {
+    return <ExternalLinkMain {...props} />;
+  },
   args: {
     href: "https://www.youtube.com/watch?v=mH-Sg-8EnxM",
     children: "Listen to my new album single",
