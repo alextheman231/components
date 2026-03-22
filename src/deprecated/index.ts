c27ac20b8f70978b5eb03c95e17eba666af618a3
AlexMan123456 <alex.man03888@gmail.com>
2026-03-22 17:33:04 +0000

    Deprecate IconWithPopover
    It causes accessibility issues, and a better option in the form of Tooltip already exists.

--- src/deprecated/index.ts
+++ src/deprecated/index.ts
@@ -1,1 +1,4 @@
 export { default as DarkModeToggle } from "src/deprecated/DarkModeToggle";
+export { default as IconWithPopover } from "src/deprecated/IconWithPopover";
+
+export type { IconWithPopoverProps } from "src/deprecated/IconWithPopover";
