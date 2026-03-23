c249400d271e5707b045603c021f77992ba855b1
AlexMan123456 <alex.man03888@gmail.com>
2026-03-23 23:33:56 +0000

    Deprecate ListItemInternalLink
    Not really worth keeping considering the rarity of the expected usage, and the fact that it's not that hard to recreate. It didn't even forward the ref.

--- src/deprecated/index.ts
+++ src/deprecated/index.ts
@@ -1,5 +1,6 @@
 export { default as DarkModeToggle } from "src/deprecated/DarkModeToggle";
 export { default as IconWithPopover } from "src/deprecated/IconWithPopover";
+export { default as ListItemInternalLink } from "src/deprecated/ListItemInternalLink";
 export { default as PopoverText } from "src/deprecated/PopoverText";
 
 export type { IconWithPopoverProps } from "src/deprecated/IconWithPopover";
