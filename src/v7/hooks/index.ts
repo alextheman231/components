9f77bc9a5ee7b172db4dac96e7a8bac1e343a4ff
AlexMan123456 <alex.man03888@gmail.com>
2026-03-26 01:00:17 +0000

    Add a few initial v7 components that are planned to integrate with TanStack form
    This may be used for the sign-in page in Lexicon, for example.

---
+++ src/v7/hooks/index.ts
@@ -0,0 +1,3 @@
+export { default as useAppForm } from "src/v7/hooks/useAppForm";
+
+export * from "src/v7/hooks/formHooks";
---
+++ stories/Form.stories.tsx
@@ -0,0 +1,87 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import Button from "@mui/material/Button";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Divider from "@mui/material/Divider";
+import { useState } from "react";
+import z from "zod";
+
+import useAppForm from "src/v7/hooks/useAppForm";
+
+const meta: Meta = {
+  title: "Form Hooks",
+};
+
+export default meta;
+type Story = StoryObj;
+
+interface DemoType {
+  firstName: string;
+  surname: string;
+}
+
+export const Form: Story = {
+  render: () => {
+    const [submitted, setSubmitted] = useState<boolean>(false);
+    const [data, setData] = useState<DemoType | null>(null);
+
+    const form = useAppForm({
+      defaultValues: { firstName: "", surname: "" },
+      onSubmit: async ({ value }) => {
+        setSubmitted(true);
+        setData(value);
+      },
+      validators: {
+        onChange: z.object({
+          firstName: z.string(),
+          surname: z.string(),
+        }),
+      },
+    });
+
+    if (!submitted) {
+      return (
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            form.handleSubmit();
+          }}
+        >
+          <CardContent>
+            <form.AppField name="firstName">
+              {(field) => {
+                return <field.TextField />;
+              }}
+            </form.AppField>
+            <form.AppField name="surname">
+              {(field) => {
+                return <field.TextField />;
+              }}
+            </form.AppField>
+          </CardContent>
+          <Divider />
+          <CardActions>
+            <form.AppForm>
+              <form.SubmitButton />
+            </form.AppForm>
+          </CardActions>
+        </form>
+      );
+    }
+
+    return (
+      <>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+        <Button
+          onClick={() => {
+            form.reset();
+            setSubmitted(false);
+          }}
+        >
+          Return to form
+        </Button>
+      </>
+    );
+  },
+};
