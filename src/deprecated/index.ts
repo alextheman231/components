4cc398b7ddab50421629d1e98dea8ef6b6e99c96
AlexMan123456 <alex.man03888@gmail.com>
2026-03-30 00:28:43 +0100

    Deprecate SubmitButton from the root entrypoint in favour of that from the v7 entrypoint

--- src/deprecated/index.ts
+++ src/deprecated/index.ts
@@ -8,6 +8,7 @@ export { default as InternalLink } from "src/deprecated/InternalLink";
 export { default as ListItemInternalLink } from "src/deprecated/ListItemInternalLink";
 export { default as NavigationDrawer } from "src/deprecated/NavigationDrawer";
 export { default as PopoverText } from "src/deprecated/PopoverText";
+export { default as SubmitButton } from "src/deprecated/SubmitButton";
 
 export * from "src/deprecated/DropdownMenu2";
 
@@ -15,3 +16,4 @@ export type { IconWithPopoverProps } from "src/deprecated/IconWithPopover";
 export type { InternalLinkProps } from "src/deprecated/InternalLink";
 export type { NavigationDrawerProps, NavMenuItem } from "src/deprecated/NavigationDrawer";
 export type { PopoverTextProps } from "src/deprecated/PopoverText";
+export type { SubmitButtonProps } from "src/deprecated/SubmitButton";
