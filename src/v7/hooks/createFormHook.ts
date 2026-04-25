92236404a24b5c84e655756068bff53e91d28bce
AlexMan123456 <alex.man03888@gmail.com>
2026-04-25 01:42:37 +0100

    Update the createFormHook typing so that it actually does extend the app form typing with the user provided components when provided

--- src/v7/hooks/createFormHook.ts
+++ src/v7/hooks/createFormHook.ts
@@ -1,22 +1,47 @@
+import type { ComponentType } from "react";
+
 import { omitProperties } from "@alextheman/utility";
 import { createFormHook as createTanstackFormHook } from "@tanstack/react-form";
 
 import { SubmitButton, TextField } from "src/v7/components";
 import { fieldContext, formContext } from "src/v7/hooks/formHooks";
 
+export interface AlexFieldComponents {
+  TextField: typeof TextField;
+}
+
+export interface AlexFormComponents {
+  SubmitButton: typeof SubmitButton;
+}
+
 /** Create the hooks for your app form with `@tanstack/react-form` using our default configuration. */
-function createFormHook(options?: Partial<Parameters<typeof createTanstackFormHook>[0]>) {
+function createFormHook<
+  FieldComponents extends Record<string, ComponentType<any>> = Record<string, never>,
+  FormComponents extends Record<string, ComponentType<any>> = Record<string, never>,
+>(
+  options?: Partial<
+    Omit<Parameters<typeof createTanstackFormHook>[0], "formComponents" | "fieldComponents"> & {
+      fieldComponents?: FieldComponents;
+      formComponents?: FormComponents;
+    }
+  >,
+): ReturnType<
+  typeof createTanstackFormHook<
+    AlexFieldComponents & FieldComponents,
+    AlexFormComponents & FormComponents
+  >
+> {
   return createTanstackFormHook({
     fieldContext,
     formContext,
     fieldComponents: {
       TextField,
       ...options?.fieldComponents,
-    },
+    } as AlexFieldComponents & FieldComponents,
     formComponents: {
       SubmitButton,
       ...options?.formComponents,
-    },
+    } as AlexFormComponents & FormComponents,
     ...omitProperties(options ?? {}, ["formComponents", "fieldComponents"]),
   });
 }
