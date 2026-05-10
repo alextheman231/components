f29f8d67b359d5450fa77489564e1bffb4fea1e6
AlexMan123456 <alex.man03888@gmail.com>
2026-05-10 01:25:40 +0100

    Add a QueryBoundaryNullable and QueryBoundaryFallback component
    These two new added components give more control over the handling of nullable data. They are now treated separately from error state in case we want to display nullable data closer to the data itself rather than with the error.

--- src/providers/QueryBoundaryProvider/index.tsx
+++ src/providers/QueryBoundaryProvider/index.tsx
@@ -3,6 +3,8 @@ import QueryBoundaryProvider from "src/providers/QueryBoundaryProvider/QueryBoun
 export { default as QueryBoundaryData } from "src/providers/QueryBoundaryProvider/QueryBoundaryData";
 export { default as QueryBoundaryDataMap } from "src/providers/QueryBoundaryProvider/QueryBoundaryDataMap";
 export { default as QueryBoundaryError } from "src/providers/QueryBoundaryProvider/QueryBoundaryError";
+export { default as QueryBoundaryFallback } from "src/providers/QueryBoundaryProvider/QueryBoundaryFallback";
+export { default as QueryBoundaryNullable } from "src/providers/QueryBoundaryProvider/QueryBoundaryNullable";
 
 export type { QueryBoundaryDataProps } from "src/providers/QueryBoundaryProvider/QueryBoundaryData";
 export type {
@@ -12,17 +14,17 @@ export type {
   QueryBoundaryDataMapPropsWithItemParser,
   QueryBoundaryDataMapPropsWithNoParser,
 } from "src/providers/QueryBoundaryProvider/QueryBoundaryDataMap";
+export default QueryBoundaryProvider;
+export type { QueryBoundaryErrorProps } from "src/providers/QueryBoundaryProvider/QueryBoundaryError";
+export type { QueryBoundaryFallbackProps } from "src/providers/QueryBoundaryProvider/QueryBoundaryFallback";
 export type {
-  QueryBoundaryErrorBaseProps,
-  QueryBoundaryErrorPropsWithNullable,
-  QueryBoundaryErrorPropsWithUndefinedOrNull,
-  QueryBoundaryErrorProps,
-} from "src/providers/QueryBoundaryProvider/QueryBoundaryError";
+  QueryBoundaryNullablePropsWithNullable,
+  QueryBoundaryNullablePropsWithUndefinedOrNull,
+  QueryBoundaryNullableProps,
+} from "src/providers/QueryBoundaryProvider/QueryBoundaryNullable";
 export type {
   QueryBoundaryProviderProps,
   QueryBoundaryProviderBaseProps,
   QueryBoundaryProviderPropsWithError,
   QueryBoundaryProviderPropsWithNoError,
 } from "src/providers/QueryBoundaryProvider/QueryBoundaryProvider";
-
-export default QueryBoundaryProvider;
