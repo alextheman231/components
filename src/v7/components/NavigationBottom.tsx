591ced4f0ec85bd4e41c2ef1b34b3c304215751c
AlexMan123456 <alex.man03888@gmail.com>
2026-05-18 01:56:11 +0100

    Add a NavigationBottom v7 component
    This swaps out the react-router-dom usage with the new Wouter usage.

---
+++ src/v7/components/NavigationBottom.tsx
@@ -0,0 +1,52 @@
+import type { JSX, ReactNode } from "react";
+
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import { useState } from "react";
+
+import { InternalLink } from "src/v7/components/routing";
+
+export interface NavItemBottom {
+  /** The value associated with the nav item. */
+  value: string;
+  /** The label to display on the nav item. */
+  label: string;
+  /** An icon to display alongside the nav item. */
+  icon?: JSX.Element;
+  /** Where in your app the nav item should navigate to. */
+  to: string;
+}
+
+export interface NavigationBottomProps {
+  /** Children to display above the nav bar. */
+  children: ReactNode;
+  /** An array of nav items to show. */
+  navItems: Array<NavItemBottom>;
+}
+
+/** Renders a navigation bar at the bottom of the screen. Especially helpful for common navigation options in a mobile app. */
+function NavigationBottom({ children, navItems }: NavigationBottomProps) {
+  const [value, setValue] = useState<string>("");
+  return (
+    <>
+      <Box sx={{ paddingBottom: 7 }}>{children}</Box>
+      <Paper sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(_, value) => {
+            setValue(value);
+          }}
+        >
+          {navItems.map((item) => {
+            return <BottomNavigationAction key={item.value} {...item} component={InternalLink} />;
+          })}
+        </BottomNavigation>
+      </Paper>
+    </>
+  );
+}
+
+export default NavigationBottom;
