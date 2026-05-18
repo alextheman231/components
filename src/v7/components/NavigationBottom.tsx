e43cfcfed7b9a581e3f029afa5b1ba646ecad653
AlexMan123456 <alex.man03888@gmail.com>
2026-05-18 23:19:03 +0100

    Ensure NavigationBottom highlights based on the current location
    Previously if you reached a nav item by some other means beyond the bottom navigation, it wouldn't highlight correctly. This was because the value and the to were separate and so there could be a mismatch because of that. This changes that so that the value of the navigation is the link, which ensures it is highlighted consistently when expected.

--- src/v7/components/NavigationBottom.tsx
+++ src/v7/components/NavigationBottom.tsx
@@ -4,19 +4,18 @@ import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
-import { useState } from "react";
 
-import { InternalLink } from "src/v7/components/routing";
+import { InternalLink, useAbsoluteLocation } from "src/v7/components/routing";
 
 export interface NavItemBottom {
-  /** The value associated with the nav item. */
-  value: string;
   /** The label to display on the nav item. */
   label: string;
   /** An icon to display alongside the nav item. */
   icon?: JSX.Element;
   /** Where in your app the nav item should navigate to. */
   to: string;
+  /** The value associated with the nav item (defaults to the `to` value). */
+  value?: string;
 }
 
 export interface NavigationBottomProps {
@@ -28,20 +27,21 @@ export interface NavigationBottomProps {
 
 /** Renders a navigation bar at the bottom of the screen. Especially helpful for common navigation options in a mobile app. */
 function NavigationBottom({ children, navItems }: NavigationBottomProps) {
-  const [value, setValue] = useState<string>("");
+  const [location] = useAbsoluteLocation();
   return (
     <>
       <Box sx={{ paddingBottom: 7 }}>{children}</Box>
       <Paper sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}>
-        <BottomNavigation
-          showLabels
-          value={value}
-          onChange={(_, value) => {
-            setValue(value);
-          }}
-        >
+        <BottomNavigation showLabels value={location}>
           {navItems.map((item) => {
-            return <BottomNavigationAction key={item.value} {...item} component={InternalLink} />;
+            return (
+              <BottomNavigationAction
+                key={item.to}
+                value={item.value ?? item.to}
+                {...item}
+                component={InternalLink}
+              />
+            );
           })}
         </BottomNavigation>
       </Paper>
