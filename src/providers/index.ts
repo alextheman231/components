600e9c6b4a58c4fce742ed71b2135e55de7977f9
AlexMan123456 <alex.man03888@gmail.com>
2026-05-20 20:14:45 +0100

    Add missing exports...

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,7 +1,10 @@
+export { default as AudioProvider, useAudioContext } from "src/providers/AudioProvider";
+export { default as AudioControls } from "src/providers/AudioProvider/AudioControls";
 export { default as ModeProvider, useMode } from "src/providers/ModeProvider";
 export { default as ScreenSizeProvider, useScreenSize } from "src/providers/ScreenSizeProvider";
 export { default as SnackbarProvider, useSnackbar } from "src/providers/SnackbarProvider";
 
+export type { AudioContextValue, AudioProviderProps, TrackData } from "src/providers/AudioProvider";
 export type { ModeProviderProps } from "src/providers/ModeProvider";
 export type { ScreenSizeProps, ScreenSizeContextValue } from "src/providers/ScreenSizeProvider";
 export type { SnackbarProviderProps } from "src/providers/SnackbarProvider";
