0d34a5968835866686bfc91795a86e4012bee335
AlexMan123456 <alex.man03888@gmail.com>
2026-05-25 23:46:44 +0100

    Export the Page props

--- src/root/components/index.ts
+++ src/root/components/index.ts
@@ -7,5 +7,6 @@ export { default as SwitchWithIcons } from "src/root/components/SwitchWithIcons"
 
 export type { CollapsibleItemProps } from "src/root/components/CollapsibleItem";
 export type { ExternalLinkProps } from "src/root/components/ExternalLink";
+export type { PageProps } from "src/root/components/Page";
 export type { SkeletonRowProps } from "src/root/components/SkeletonRow";
 export type { SwitchWithIconsProps } from "src/root/components/SwitchWithIcons";
