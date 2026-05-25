a53372106caa6fc5239ac8097b72f131d4b72537
AlexMan123456 <alex.man03888@gmail.com>
2026-05-26 00:50:29 +0100

    Add missing Snackbars export
    UGGGGGGGGGGH!

--- src/snackbar/index.ts
+++ src/snackbar/index.ts
@@ -1,3 +1,4 @@
 export { default as SnackbarProvider, useSnackbarContext } from "src/snackbar/SnackbarProvider";
+export { default as Snackbars } from "src/snackbar/Snackbars";
 
 export type { SnackbarProviderProps } from "src/snackbar/SnackbarProvider";
