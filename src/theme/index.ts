9db2271404f1e11c45779e727370b025b94fdbaa
AlexMan123456 <alex.man03888@gmail.com>
2026-05-26 01:25:29 +0100

    Dude I just wanna scream

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,3 +1,4 @@
 export { default as ThemeProvider, useThemeContext } from "src/theme/ThemeProvider";
+export { default as ThemeToggle } from "src/theme/ThemeToggle";
 
 export type { ThemeProviderProps, ThemeContextValue } from "src/theme/ThemeProvider";
