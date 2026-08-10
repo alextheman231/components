4a21bf4ac9c17aaaba4469c8ed5ad103730b6d25
AlexMan123456 <alex.man03888@gmail.com>
2026-08-10 01:35:40 +0100

    Add a Search component
    This component is expected to be used in Lexicon, for example to search for a specific blog. It works using a button to trigger the search rather than updating with the state, as that feels better for content-based platforms such as Lexicon.

--- src/root/components/index.ts
+++ src/root/components/index.ts
@@ -2,11 +2,13 @@ export { default as Artwork } from "src/root/components/Artwork";
 export { default as CollapsibleItem } from "src/root/components/CollapsibleItem";
 export { default as ExternalLink } from "src/root/components/ExternalLink";
 export { default as Page } from "src/root/components/Page";
+export { default as Search } from "src/root/components/Search";
 export { default as SkeletonRow } from "src/root/components/SkeletonRow";
 export { default as SwitchWithIcons } from "src/root/components/SwitchWithIcons";
 
 export type { CollapsibleItemProps } from "src/root/components/CollapsibleItem";
 export type { ExternalLinkProps } from "src/root/components/ExternalLink";
 export type { PageProps } from "src/root/components/Page";
+export type { SearchProps, SearchBaseProps } from "src/root/components/Search";
 export type { SkeletonRowProps } from "src/root/components/SkeletonRow";
 export type { SwitchWithIconsProps } from "src/root/components/SwitchWithIcons";
